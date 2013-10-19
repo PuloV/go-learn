dd5c845fe59ed0b390232293cc5d678446706385
Nedyalko Dyakov <me@ndyakov.eu>
2013-10-19 22:13:41 +0300

    One more test for the second challenge

--- challenges/DragonFractal/solution_test.go
+++ challenges/DragonFractal/solution_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 var expected [15]string = [15]string{"up", "left", "down", "left", "down", "right", "down", "left", "down", "right", "up", "right", "down", "right", "down"}
 
-func TestFirstFiveteen(t *testing.T) {
+func TestFirstFifteen(t *testing.T) {
 	fractal := new(DragonFractal)
 
 	for i:=0; i<15; i+=1 {
@@ -11,5 +11,16 @@ func TestFirstFiveteen(t *testing.T) {
 		if result != expected[i] {
 			t.Errorf("Expected `%s` but got `%s` for step `%d`",expected[i], result, i); 
 		}
+	}
+}
+func TestFifty(t *testing.T) {
+	fractal := new(DragonFractal)
+	
+	for i:=0; i<49; i+=1 {
+		fractal.Next()
+	}
+	result := fractal.Next()
+	if result != "right" {
+		t.Errorf("Expected `%s` but got `%s` for step `%d`", "right", result, 50); 
 	}
 }
